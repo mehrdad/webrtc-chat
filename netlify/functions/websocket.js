b8b2eb7ac5ad0791d2ad336a54900effe0b77935
Mehrdad Rashidfarrukhi <maddog137@gmail.com>
2025-01-23 16:10:44 -0800

    modified websocket

--- netlify/functions/websocket.js
+++ netlify/functions/websocket.js
@@ -1,26 +1,24 @@
-import { Server } from "socket.io";
+const { Server } = require("socket.io");
 
-const rooms = {};
-
-export async function handler(event, context) {
-  const io = new Server(event.httpMethod === 'GET' ? { path: '/ws' } : undefined);
+exports.handler = async (event) => {
+  const io = new Server({
+    cors: {
+      origin: "*",
+      methods: ["GET", "POST"]
+    }
+  });
 
   io.on('connection', (socket) => {
     const roomId = socket.handshake.query.room;
-
     socket.join(roomId);
-    
+
     socket.on('signal', (data) => {
       socket.to(roomId).emit('signal', data);
-    });
-
-    socket.on('disconnect', () => {
-      socket.leave(roomId);
     });
   });
 
   return {
     statusCode: 200,
-    body: 'WebSocket connected'
+    body: 'Socket.IO ready'
   };
-}
+};
